ae8b9cbf3d234805bb2bd1db04776422e2dc2323
ceui <991807272@qq.com>
2022-01-07 05:29:12 +0800

    add sub router

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const Koa = require('koa');
 const Router = require('koa-router')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const users = require('./routes/api/users')
+
 const mongooseURI = keys.mongooseURI
 
 mongoose.connect(mongooseURI, () => {
     console.log('mongoose connected...')
 })
 
+
 const router = new Router()
 const app = new Koa();
 
-app.use(router.routes)
+router.use('/api/users', users)
+app.use(router.routes())
 
 const port = process.env.PORT || 3000
 
---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const Router = require('koa-router')
+
+const router = new Router();
+
+router.get('/test', async ctx => {
+    ctx.status = 200;
+    ctx.body = {
+        msg: 'users works'
+    }
+})
+
+module.exports = router.routes();
