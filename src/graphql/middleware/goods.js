615ffb62ff8434ad1b60ae2e5e9db8a4625db052
losidk <991807272@qq.com>
2022-03-21 06:50:36 +0800

    temporary commit with the goods graphql middleware

--- src/graphql/middleware/goods.js
+++ src/graphql/middleware/goods.js
@@ -10,22 +10,22 @@ const checkUploadedFileType = async (resolve, parent, args, context, info) => {
 }
 
 // goods validator
-const publishValidator = async (resolve, parent, args, context, info) => {
-  const types = { string: { type: 'string' }, number: { type: 'number' } }
-  const requiredEnum = { required: { required: true }, notRequired: { required: false } }
-  try {
-    // name: goods name;image: goods image;count: goods count;price: goods price
-    ctx.verifyParams({
-      name: { ...types.string, ...requiredEnum.required },
-      image: { ...types.string, ...requiredEnum.required },
-      count: { ...types.number, ...requiredEnum.required },
-      price: { ...types.number, ...requiredEnum.required }
-    })
-  } catch (e) {
-    ctx.status = HTTP_CODE.SUCCESS
-    ctx.body = { ...GOODS_ERRORS.GOODS_PUBLISH_VALIDATE_ERRORS, data: e.errors }
-    return ctx.app.emit('error', e, ctx)
-  }
-
-  await next()
-}
+// const publishValidator = async (resolve, parent, args, context, info) => {
+//   const types = { string: { type: 'string' }, number: { type: 'number' } }
+//   const requiredEnum = { required: { required: true }, notRequired: { required: false } }
+//   try {
+//     // name: goods name;image: goods image;count: goods count;price: goods price
+//     ctx.verifyParams({
+//       name: { ...types.string, ...requiredEnum.required },
+//       image: { ...types.string, ...requiredEnum.required },
+//       count: { ...types.number, ...requiredEnum.required },
+//       price: { ...types.number, ...requiredEnum.required }
+//     })
+//   } catch (e) {
+//     ctx.status = HTTP_CODE.SUCCESS
+//     ctx.body = { ...GOODS_ERRORS.GOODS_PUBLISH_VALIDATE_ERRORS, data: e.errors }
+//     return ctx.app.emit('error', e, ctx)
+//   }
+//
+//   await next()
+// }
